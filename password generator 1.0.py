d2daef9a362c45f360586bca604e23751f230bd6
nikolasprogamer <83187713+nikolasprogamer@users.noreply.github.com>
2021-05-05 19:45:21 +0300

    PASSWORD GENERATOR 1.0

---
+++ password generator 1.0.py
@@ -0,0 +1,54 @@
+print("Bot: Welcome to our password generator! What's your name?")
+user_name = input()
+print("You: " +user_name)
+print("Bot: Nice to meet you, " +user_name)
+print("Bot: How strong would you like your password to be?")
+print("If easy, type easy")
+print("If medium, type medium")
+print("If strong, type strong")
+pass_def = input()
+if pass_def == "easy":
+    print(user_name +": " +"easy")
+    print("1. haxug1902")
+    print("2. plafgj74!")
+    print("3. omgrtx47?!")
+    print("4. oftag?!9")
+    print("Bot: more passwords comming soon!")
+    user_input = input()
+    if user_input == "info":
+        print("version: 1.0")
+    elif user_input == "version":
+        print("version: 1.0")
+    else:
+        print("I can't understand what are you saying")
+    user_input = input()
+    if user_input == "info":
+        print("version: 1.0")
+    elif user_input == "version":
+        print("version: 1.0")
+    else:
+        print("I can't understand what are you saying")
+        print("please write restart to restart the project")
+        user_input = input()
+        if user_input == "restart":
+            print("Bot: Welcome to back to our password generator, " +user_name)
+print("Bot: Nice to meet you, " +user_name)
+print("Bot: How strong would you like your password to be?")
+print("If easy, type easy")
+print("If medium, type medium")
+print("If strong, type strong")
+pass_def = input()
+if pass_def == "easy":
+    print(user_name +": " +"easy")
+    print("1. haxug1902")
+    print("2. plafgj74!")
+    print("3. omgrtx47?!")
+    print("4. oftag?!9")
+    print("Bot: more passwords comming soon!")
+    print("Press enter to quit")
+    user_input = input()
+        
+    
+    
+
+    
